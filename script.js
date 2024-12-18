7635efa45d89187272a4cf6b018322760762490a
Dementiy <masyura.dima@mail.ru>
2024-12-18 23:48:19 +0300

    lastCommit

--- script.js
+++ script.js
@@ -1,21 +1,35 @@
 document.addEventListener("DOMContentLoaded", () => {
-    const routes = document.querySelectorAll(".route"); // Изображения рутов
-    const paths = document.querySelectorAll("path"); // Пути для отрисовки
-
-    // Анимация путей SVG
-    paths.forEach((path, index) => {
-      path.style.animationDelay = `${index * 2}s`; // Задержка для каждого пути
-    });
-
-    // Анимация появления изображений
+    const routes = document.querySelectorAll(".route"); 
+    const paths = document.querySelectorAll("path"); 
+    
     routes.forEach((route, index) => {
         setTimeout(() => {
-            route.style.opacity = 1; // Показать изображение
+            route.style.opacity = 1; 
             setTimeout(() => {
-                route.style.opacity = 0; // Скрыть изображение после анимации
-            }, 2000); // Продолжительность показа совпадает с анимацией пути
-        }, index * 2000); // Задержка перед показом следующего изображения
+            }, 2000);
+        }, index * 2000); 
     });
 });
 
-/*Анимация 2 */
+
+
+/*Анимация 3 */
+const inputField = document.getElementById('name');
+const inputLabel = document.querySelector('.input-label');
+
+inputField.addEventListener('focus', () => {
+    inputLabel.style.top = '-20px';
+    inputLabel.style.left = '50%';
+    inputLabel.style.transform = 'translateX(-50%)';
+    inputLabel.style.fontSize = '14px';
+});
+
+inputField.addEventListener('blur', () => {
+    if (!inputField.value) {
+        inputLabel.style.top = '10px';
+        inputLabel.style.left = '10px';
+        inputLabel.style.transform = 'translateX(0)';
+        inputLabel.style.fontSize = '16px';
+    }
+});
+
